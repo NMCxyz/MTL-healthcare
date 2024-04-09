6cc5161453744b3d5ceeebd9f47f4c642b4b8ea7
NMCxyz <cuong220103@gmail.com>
2024-04-09 18:14:10 +0700

    add RNN model

--- net.py
+++ net.py
@@ -95,3 +95,68 @@ class RegressionLSTM(nn.Module):
         reg_out = self.reg(out[:, -1, :]).flatten()
 
         return reg_out
+
+
+# ------------------ RNN ------------------------
+
+class ClassifyRNN(nn.Module):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size, dropout):
+        super(ClassifyRNN, self).__init__()
+        self.rnn1 = nn.RNN(input_size, hidden_size_1, batch_first=True)
+        self.rnn2 = nn.RNN(hidden_size_1, hidden_size_2, batch_first=True)
+        self.dropout = nn.Dropout(dropout)
+        self.cls = nn.Linear(hidden_size_2, output_size)
+
+    def forward(self, x):
+        out, _ = self.rnn1(x)
+        out, _ = self.rnn2(out)
+        out = self.dropout(out)
+        out = out.contiguous().view(-1, out.size(-1)) 
+        out = self.cls(out)
+        return out
+
+class RegressionRNN(nn.Module):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size, dropout):
+        super(RegressionRNN, self).__init__()
+        self.rnn1 = nn.RNN(input_size, hidden_size_1, batch_first=True)
+        self.rnn2 = nn.RNN(hidden_size_1, hidden_size_2, batch_first=True)
+        self.dropout = nn.Dropout(dropout)
+        self.reg = nn.Linear(hidden_size_2, 1)
+
+    def forward(self, x):
+        out, _ = self.rnn1(x)
+        out, _ = self.rnn2(out)
+        out = self.dropout(out)
+        out = out[:, -1, :]
+        reg_out = self.reg(out).squeeze()  
+        return reg_out
+
+class MultitaskRNN(nn.Module):
+    def __init__(
+            self,
+            input_size,
+            hidden_size_1,
+            hidden_size_2,
+            output_size,
+            dropout
+        ):
+        super(MultitaskRNN, self).__init__()
+
+        self.rnn = nn.RNN(input_size, hidden_size_1, batch_first=True)
+        self.rnn2 = nn.RNN(hidden_size_1, hidden_size_2, batch_first=True)
+        self.dropout = nn.Dropout(dropout)
+        self.cls = nn.Linear(hidden_size_1, output_size)
+        self.reg = nn.Linear(hidden_size_2, 1)
+
+    def forward(self, x):
+        out, _ = self.rnn(x)
+        cls_out = out.contiguous()
+        cls_out = cls_out.view(-1, cls_out.size(-1)) 
+        cls_out = self.cls(cls_out)
+
+        out, _ = self.rnn2(out)
+        out = self.dropout(out)
+        out = out[:, -1, :]
+        reg_out = self.reg(out).squeeze()  
+
+        return reg_out, cls_out
--- train_multitask_orthogonal.py
+++ train_multitask_orthogonal.py
@@ -9,7 +9,7 @@ from dataset import MultitaskDataset
 # from trainer import MultitaskTrainer
 from trainer.multitask_orthogonal_trainer import MultitaskOrthogonalTrainer
 from net import (
-    MultitaskLSTM,
+    MultitaskRNN,
     cls_metric,
     cls_loss_fn,
     reg_loss_fn,
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     # Initialize the PyTorch model
-    model = MultitaskLSTM(
+    model = MultitaskRNN(
         input_size=args.input_dim,
         hidden_size_1=args.n_hidden_1,
         hidden_size_2=args.n_hidden_2,
